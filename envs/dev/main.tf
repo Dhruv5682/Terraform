d1e28227f5e44d8310a5254d4dde0861b2c97524
Dhruv Sukhadiya <dhruv.sukhadiya@simformsolutions.com>
2026-06-12 11:31:10 +0530

    Hello, more effective

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -19,11 +19,19 @@ provider "azurerm" {
   }
 }
 
+locals {
+  common_tags = {
+    environment = var.environment
+    region      = var.location
+  }
+}
+
 module "resourcegroup" {
   source      = "../../modules/resourcegroup"
   name        = var.rgname
   location    = var.location
   environment = var.environment
+  tags        = local.common_tags
 }
 
 module "infrastructure" {
@@ -35,10 +43,11 @@ module "infrastructure" {
   appgw_subnet_prefix      = "10.1.1.0/24"
   appservice_subnet_prefix = "10.1.2.0/24"
   pe_subnet_prefix         = "10.1.3.0/24"
-  acr_name                 = "acrwebappdev${random_string.suffix.result}"
-  kv_name                  = "kv-webapp-${random_string.suffix.result}"
-  log_workspace_name       = "law-webapp-dev"
-  app_insights_name        = "appi-webapp-dev"
+  acr_name                 = "${var.acr_name}${random_string.suffix.result}"
+  kv_name                  = "${var.kv_name}-${random_string.suffix.result}"
+  log_workspace_name       = var.log_workspace_name
+  app_insights_name        = var.app_insights_name
+  tags                     = local.common_tags
 }
 
 module "database" {
@@ -47,36 +56,41 @@ module "database" {
   resource_group_name = module.resourcegroup.namee
   vnet_id             = module.infrastructure.vnet_id
   pe_subnet_id        = module.infrastructure.pe_subnet_id
-  mysql_server_name   = "mysql-webapp-${random_string.suffix.result}"
+  mysql_server_name   = "${var.mysql_server_name}-${random_string.suffix.result}"
   admin_username      = var.db_admin_user
   admin_password      = var.db_admin_password
+  tags                = local.common_tags
 }
 
 module "compute" {
   source               = "../../modules/compute"
   location             = var.location
   resource_group_name  = module.resourcegroup.namee
-  app_plan_name        = "plan-webapp-dev"
-  frontend_app_name    = "app-frontend-${random_string.suffix.result}"
-  backend_app_name     = "app-backend-${random_string.suffix.result}"
+  app_plan_name        = var.app_plan_name
+  frontend_app_name    = "${var.frontend_app_name}-${random_string.suffix.result}"
+  backend_app_name     = "${var.backend_app_name}-${random_string.suffix.result}"
   acr_login_server     = module.infrastructure.acr_login_server
   acr_id               = module.infrastructure.acr_id
   appservice_subnet_id = module.infrastructure.appservice_subnet_id
   kv_id                = module.infrastructure.kv_id
   kv_uri               = module.infrastructure.kv_uri
   mysql_fqdn           = module.database.mysql_server_fqdn
-  db_user              = var.db_admin_user
-  db_password          = var.db_admin_password
+  db_user                        = var.db_admin_user
+  db_password                    = var.db_admin_password
+  app_insights_connection_string = module.infrastructure.app_insights_connection_string
+  log_workspace_id               = module.infrastructure.log_workspace_id
+  tags                           = local.common_tags
 }
 
 module "gateway" {
   source                = "../../modules/gateway"
   location              = var.location
   resource_group_name   = module.resourcegroup.namee
-  appgw_name            = "agw-webapp-dev"
+  appgw_name            = var.appgw_name
   appgw_subnet_id       = module.infrastructure.appgw_subnet_id
   frontend_app_hostname = module.compute.frontend_default_hostname
   backend_app_hostname  = module.compute.backend_default_hostname
+  tags                  = local.common_tags
 }
 
 resource "random_string" "suffix" {
@@ -84,3 +98,11 @@ resource "random_string" "suffix" {
   special = false
   upper   = false
 }
+
+module "policy" {
+  source                   = "../../modules/policy"
+  resource_group_id        = module.resourcegroup.ids
+  location                 = var.location
+  allowed_app_service_skus = ["B1", "B2", "B3"]
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -28,3 +28,48 @@ variable "db_admin_password" {
   type      = string
   sensitive = true
 }
+
+variable "acr_name" {
+  type    = string
+  default = "acrwebappdevsea"
+}
+
+variable "kv_name" {
+  type    = string
+  default = "kv-webapp-dev-sea"
+}
+
+variable "log_workspace_name" {
+  type    = string
+  default = "law-webapp-dev-sea"
+}
+
+variable "app_insights_name" {
+  type    = string
+  default = "appi-webapp-dev-sea"
+}
+
+variable "mysql_server_name" {
+  type    = string
+  default = "mysql-webapp-dev-sea"
+}
+
+variable "app_plan_name" {
+  type    = string
+  default = "plan-webapp-dev-sea"
+}
+
+variable "frontend_app_name" {
+  type    = string
+  default = "app-frontend-dev-sea"
+}
+
+variable "backend_app_name" {
+  type    = string
+  default = "app-backend-dev-sea"
+}
+
+variable "appgw_name" {
+  type    = string
+  default = "agw-webapp-dev-sea"
+}
